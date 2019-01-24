e10e96e7e070b4874f05285f43b8af4680121bdb
4season <junhoheo11@gmail.com>
2019-01-24 09:18:42 +0900

    Hm...

--- bot.js
+++ bot.js
@@ -1,5 +1,4 @@
 const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot(getToken(), {polling: true});
 
 const getToken = (function(){
     const token = process.env.TELEGRAM_TOKEN;
@@ -8,12 +7,12 @@ const getToken = (function(){
     };
 })();
 
-/*
-var Aa = "오늘의 한강 수온은...";
-var Bb = "도네요. 함께가자!";
-*/
+//var Aa = "오늘의 한강 수온은...";
+//var Bb = "도네요. 함께가자!";
 
-bot.onText(/\/abc (.+)/, (msg, match) => {
+const bot = new TelegramBot(getToken(), {polling: true});
+
+bot.onText(/\/hello (.+)/, (msg, match) => {
 
     const chatId = msg.chat.id;
     const resp = match[1];
