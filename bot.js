e6a851ae7ad0590a3c4482dd442acf2115bd92ad
4season <junhoheo11@gmail.com>
2019-01-25 01:00:01 +0900

    haha...

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(getToken(), {polling: true});
-
+const request = require('request');
+const chatld = msg.chat.id;
+const router = express.Router();
 const getToken = (function(){
     const token = process.env.TELEGRAM_TOKEN;
     return function() {
@@ -12,29 +14,19 @@ var Aa = "오늘의 한강 수온은...";
 var Bb = "도입니다.\nWould you want to join me?";
 var arrNumber = new Array();
 
-const router = express.Router();
-const request = require('request');
-  
-request("https://www.wpws.kr/hangang/", function(error, response, body){
-    	
-	arrNumber[0] = body;
-	
-});
-
 bot.onText(/\/today_hangang_temperature/, (msg) => {
+
+	request("https://www.wpws.kr/hangang/", function(error, response, body){
+		
+	bot.sendMessage(chatld, body);
 	
-	const chatld = msg.chat.id;
-	
-	bot.sendMessage(chatld, arrNumber[0]);
-	arrNumber.length = 0;
-	
+	});
 });
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
-
-    const chatId = msg.chat.id;
+	
     const resp = match[1];
-
+    
     bot.sendMessage(chatId, resp);
     
 });
