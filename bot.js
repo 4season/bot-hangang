7395ba115c54fb9565745b65bd2343d3728a4e34
FourSeason <junhoheo11@gmail.com>
2019-01-25 02:37:52 +0900

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
-const m1 = require('request');
-const m2 = require('cheerio');
-const m3 = require('iconv-lite');
+const request = require('request');
+const cheerio = require('cheerio');
+const iconv = require('iconv-lite');
 
 const bot = new TelegramBot(getToken(), {polling: true});
 const getToken = (function(){
@@ -14,12 +14,21 @@ const getToken = (function(){
 var Aa = "오늘의 한강 수온은...";
 var Bb = "도입니다.";
 
+var requestOptions = { 
+    method: "GET" ,uri: "https://www.wpws.kr/hangang/" ,
+    headers: { "User-Agent": "Mozilla/5.0" } ,
+    encoding: null 
+};
+
+
 bot.onText(/\/today_hangang_temperature/, (msg) => {
-    m1.get({uri:"https://www.wpws.kr/hangang/"}, function (error, response, body) {
+    request(requestOptions, function(error, response, body) {
 
+        var strContents = new Buffer(body);
         const chatld = msg.chat.id;
+        
+        bot.sendMessage(chatld, iconv.decode(strContents, 'EUC-KR').toString()); 
 
-        bot.sendMessage(chatld, body);
     });
 });
 
