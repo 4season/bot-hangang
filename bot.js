31c0cc706b8c47cb1f286d618f6501c9ed271d02
4season <junhoheo11@gmail.com>
2019-01-24 10:38:09 +0900

    hmm

--- bot.js
+++ bot.js
@@ -9,13 +9,37 @@ const getToken = (function(){
 
 var Aa = "오늘의 한강 수온은...";
 var Bb = "도네요. 함께가자!";
+var http = require('http');
+var cheerio = require('cheerio');
+var options = {
+    hostname: 'www.wpws.kr/hangang'
+  };
 
 const bot = new TelegramBot(getToken(), {polling: true});
+
+function handleResponse(response) {
+  var serverData = '';
+  response.on('data', function (chunk) {
+    serverData += chunk;
+  });
+  response.on('end', function () {
+ 
+    var $ = cheerio.load(serverData);
+ 
+    var result = $(".bash").text();            // 클래스가 bash인 요소를 선택
+    var result2 = result.replace(/(^\s+|\s+$)/g, ""); // 앞뒤의 화이트 스페이스를 제거
+    console.log("Find by class : bash -> " + result2);
+ 
+    result = $("#AUTHOR").text();                     // id가 AUTHOR인 요소를 선택
+    result2 = result.replace(/(^\s+|\s+$)/g, "");     // 앞뒤의 화이트 스페이스를 제거
+    console.log("Find by id : AUTHOR -> " + result2);
+  });
+}
 
 bot.onText(/\/today_hangang_temperature/, (msg) => {
 	const chatld = msg.chat.id;
 	
-	bot.sendMessage(chatld, Aa + Bb);
+	bot.sendMessage(chatld, handleResponse(response));
 });
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
