14a15f5085b4182bbcfbc3f541e7669d33abeec6
4season <junhoheo11@gmail.com>
2019-01-25 16:15:27 +0900

    update

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const request = require('request');
 const cheerio = require('cheerio');
 const iconv = require('iconv-lite');
+const fetch = require('node-fetch');
 
 const bot = new TelegramBot(getToken(), {polling: true});
 const getToken = (function(){
@@ -12,25 +13,23 @@ const getToken = (function(){
 })();
 
 var Aa = "오늘의 한강 수온은...";
-var Bb = "도입니다.";
-
-var requestOptions = { 
-    method: "GET" ,
-    uri: "https://www.wpws.kr/hangang/" ,
-    headers: { "User-Agent": "Mozilla/5.0" } ,
-    encoding: null 
-};
-
+var Bb = "도입니다. 혹시 시간되신다면 저랑 함께하실래요?";
+var Cc = "오늘은 날이 아닌가봅니다... 독서실이나 가죠.";
 
 bot.onText(/\/today_hangang_temperature/, (msg) => {
-    request(requestOptions, function(error, response, body) {
-
-        var strContents = new Buffer(body);
-        const chatld = msg.chat.id;
-
-        bot.sendMessage(chatld, iconv.decode(strContents, 'EUC-KR').toString()); 
-
-    });
+	
+	const chatld = msg.chat.id;
+	
+	request('node-fetch')('http://hangang.dkserver.wo.tc/', function(error, response, body) {
+		.then(res => res.json());
+		.then(json => {
+			if(json.result) {
+				bot.sendMessage(chatld, Aa + {json["temp"]} + Bb);
+				}
+				else {
+					bot.sendMessage(chatld, Cc);
+					}
+			});
 });
 
 
