ec90925d7ab169cfbf2c56efa3392c144a8f209c
FourSeason <junhoheo11@gmail.com>
2019-01-26 01:40:02 +0900

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
-//const request = require('request');
+const request = require('request');
 //const cheerio = require('cheerio');
 //const iconv = require('iconv-lite');
 //const fetch = require('node-fetch');
@@ -16,24 +16,19 @@ var Aa = "오늘의 한강 수온은...";
 var Bb = "도입니다. 혹시 시간되신다면 저랑 함께하실래요?";
 var Cc = "오늘은 날이 아닌가봅니다... 독서실이나 가죠.";
 
-/*
 bot.onText(/\/today_hangang_temperature/, (msg) => {
 	
 	const chatld = msg.chat.id;
 	
-	request('node-fetch')('http://hangang.dkserver.wo.tc/', function(error, response, body) {
-        fetch.then(res => res.json())
-		fetch.then(json => {
-			if(json.result) {
-                bot.sendMessage(chatld, Aa + `${json["temp"]}` + Bb);
-            }
-            else {
-                bot.sendMessage(chatld, Cc);
-            }
-        });
+	request('node-fetch')('http://hangang.dkserver.wo.tc/').then(res => res.json()).then(json => {
+        if(json.result) {
+            bot.sendMessage(chatld, Aa + `${json["temp"]}` + Bb);
+        }
+        else {
+            bot.sendMessage(chatld, Cc);
+        }
     });
 });
-*/
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
     
@@ -41,5 +36,4 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
     const resp = match[1];
     
     bot.sendMessage(chatId, resp);
-    
 });
